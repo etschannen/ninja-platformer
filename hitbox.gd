e874e5ffac4f3c0aee97e5d8a174eb5b03062a2c
Evan Tschannen <ejtschannen@gmail.com>
2026-01-24 15:32:38 -0800

    spawn a random number of powerups with random durations

--- hitbox.gd
+++ hitbox.gd
@@ -9,7 +9,21 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area_2d: Area2D) -> void:
+	process_hit(area_2d)
+	
+func process_hit(area_2d: Area2D) -> void:
 	assert(area_2d is Hurtbox, "The hitbox detected an area that wasn't a hurtbox.")
 	var hurtbox = area_2d as Hurtbox
 	hurtbox.take_hit(self, false)
 	hit.emit(hurtbox)
+	
+func recheck():
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsShapeQueryParameters2D.new()
+	query.shape = $CollisionShape2D.shape
+	query.transform = global_transform
+	query.collision_mask = collision_mask
+	query.collide_with_areas = true
+
+	for obj in space_state.intersect_shape(query):
+		process_hit(obj["collider"])
--- powerup.gd
+++ powerup.gd
@@ -5,6 +5,10 @@ extends Node2D
 @onready var hitbox: Hitbox = $Hitbox
 @onready var hurtbox: Hurtbox = $Hurtbox
 
+var spawn_duration = 0.0
+var full_spawn_duration = 0.0
+var timer_color = Color8(0,0,0)
+
 func _ready() -> void:
 	powerup.scale = Vector2(Globals.default_scale, Globals.default_scale)
 	hitbox.body_entered.connect(func(body: Node2D):
@@ -16,7 +20,36 @@ func _ready() -> void:
 	hurtbox.hurt.connect(func(other_hitbox: Hitbox):
 		pass
 	)
+	
+func _physics_process(delta: float) -> void:
+	if spawn_duration > 0.0:
+		spawn_duration -= delta
+		queue_redraw()
+	elif spawn_duration < 0.0:
+		spawn_duration = 0.0
+		animated_sprite_2d.material.set_shader_parameter("alpha", 1.0)
+		hitbox.set_collision_mask_value(1,true)
+		hitbox.set_collision_mask_value(4,true)
+		hurtbox.set_collision_layer_value(5,true)
+		hitbox.recheck()
 
-func set_powerup_type(powerup):
+func set_powerup_type(powerup, duration):
 	hitbox.powerup = powerup
+	spawn_duration = duration
+	full_spawn_duration = spawn_duration
 	animated_sprite_2d.material.set_shader_parameter("hat_end_color", Globals.get_powerup_color(powerup))
+	if spawn_duration >= 0:
+		animated_sprite_2d.material.set_shader_parameter("alpha", 0.5)
+		hitbox.set_collision_mask_value(1,false)
+		hitbox.set_collision_mask_value(4,false)
+		hurtbox.set_collision_layer_value(5,false)
+		if spawn_duration <= 3.0:
+			timer_color = Color8(0,255,0,127)
+		elif spawn_duration <= 6.0:
+			timer_color = Color8(255,255,0,127)
+		else:
+			timer_color = Color8(255,0,0,127)
+
+func _draw() -> void:
+	if spawn_duration > 0.0: 
+		draw_arc(Vector2(0,1), 7, 0, 2*spawn_duration/full_spawn_duration*PI, 30, timer_color, 2)
